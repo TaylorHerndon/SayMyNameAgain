1a7a3de3517b45e14204ac0c658666e2bc2952f8
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-01-25 08:43:00 -0700

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,136 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Name As String = ""
+
+        Console.WindowHeight = Console.LargestWindowHeight
+        Console.WindowWidth = Console.LargestWindowWidth
+        Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight)
+        Console.SetWindowPosition(0, 0)
+
+LineStart:
+
+        Console.WriteLine("Oi, ello again!")
+        System.Threading.Thread.Sleep(1000)
+        Console.WriteLine("I think I've done forgot yer name ehh")
+        System.Threading.Thread.Sleep(2000)
+        Console.WriteLine("What was it again?")
+
+        Name = Console.ReadLine()
+        Console.Clear()
+
+        Select Case Name
+
+            Case "Emily"
+
+                Console.WriteLine("Oi, ello again emily!")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Been such a long time I hardley reconized ya!")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("So, ow are you an yer family doing?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Oi don't bother... ow bout me and you make a painting huh?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Press any key to start painting...")
+                Console.ReadKey()
+
+            Case "Joe"
+
+                Console.WriteLine("Oi, Joe!")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Ow en the world did I not reconize ye?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("I spose I'm really losing it in me old age.")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Oi, I know! Ow bout me and you head out painting?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("No, we won't be tagging any omes this time round.")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Well just get ourselves a canvas an paint oer a beer!")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("What do ye say?")
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Press any key to start painting...")
+                Console.ReadKey()
+
+            Case "Taylor"
+
+                Console.WriteLine("Oi, Taylor ye say?")
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("Aren't ye the poor bloke who made this dinky program?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Quite bland to be honest...")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Could use a wee bit of art on the walls.")
+                System.Threading.Thread.Sleep(1500)
+                Console.WriteLine("How bout that, eh?")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Press any key to start painting...")
+                Console.ReadKey()
+
+            Case Else
+
+                Console.WriteLine("Eh? No, no, I don't know ye. Run along now.")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Ye can head on outa ere now.")
+                System.Threading.Thread.Sleep(2000)
+                Console.WriteLine("Press any key to return to the start...")
+                Console.ReadKey()
+                GoTo LineStart
+
+        End Select
+
+        Console.Clear()
+        Console.WriteLine(",                                                                                                                                                                                            ,(//(*((*(/
+,                                                                                                                                                                                            .#((//(/#*(
+,                                                                                                                                                                                             ##(((/%%(%
+,                                                                                                                                                                                             *(%((#((((
+,                                                                        ..                                                                                                                    %&(%#%(##
+,                                                                  #&*,*. .  .,.,/#,                                                                                                           #&#(((#&&
+,                                                               *%/., .   .. ..  .. .(#/.                                                                                                      (###%##&%
+,                                                              # *..   .              .*,@@.                                                                                                   /@&%&@%%%
+,                                                             @#.    #@@,#                 ,#(%#/(//(,,.                                                                                       ,##%%&#%%
+,                                                            /,/     //((.                %/  ,       .*..,(###%(*.                                                                            .&%%%&%(&
+,                                                            (*, .,,                    .&, ..   .      .           .(%#,                                                                       @@%####%
+,                                                          (@%,..                      /*.,.   .        . .               .(/*                                                                  (@@%%%##
+,                                                        /@%,, (*   ..                *//. .     .   ,..                      .//.                                                              .@%%###(
+,                                                      .@&*.#(  ,#/(,  ...*,..       .(,..  . ..    .                             .*,.                                                           &@%(##%
+,                                                     .#.%%/&(     .(&&/ .,. .       (/..    .    . .                                .**.                                                        %@%(##(
+,                                                     ..                %@..#, .     %,. .. .  ...  .                                    ,*                                                      (@%(/*/
+,                                                                         %&  .      (.. .  . . . .     .     .                        ./   ,,                                                   *@&*,..
+,                                                                           (%  *.    &.   . .   ..                            .    .,,      . *,                                                .@@#  .
+,                                                                            (&,./,    ( .     ,   ....                      ,,                  .(.                                              @@&. .
+,                                                                             *@ . ,,   (. ..    .     *.                              * ,      ,.  ,/                                            #@@, .
+,                                                                              /@*     ,..(  .                       ..,,,,        .*/*,((    .  *     /.                                         .@@/ .
+,                                                                               .&(&*      ,(. ..                .               *   ..    .,*(/.    ,. ..                                         @@( .
+,                                                                                 #@#%,       @%     ..         ./       (        @.         *&(.#@&@&/   *%(                                      %@% .
+,                                                                                  *@(*#(        ./@&,.  . .         /&#/.  ,**,.,    ,(**&#/*,.         ..,,  ,//*                                (@& .
+,                                                                                    .(#%(#/.         ,(&@@&(/*. .,...,*,//*(#&#%%(/*..          ..               .,,.,,.                          /@@,.
+,                                                                                       .(&(/%.                ,,(/*,,...   .,,/**,,,.... .. .,             .     . .   .*(**.                     .@@#.
+,                                    /                                                      ./*(**                                              *. ..        ..             . ,((*,.                @@&.
+,                                     .%   ,@*                                                   *(* ..                                         .        ...,,.     ..      ...    ../**.           /@@(
+,                                      */* &    ,                                                     ,(%%((//*/*..        .. ./((##%%#%(((*/,,,,,..  .  .. .      ..**,...,....      ,   ,,.       .@@%
+,                                  *,   #%,#   %.                                                            .*.   @. . ,,.,,,,,. . .,,/(&@@@(,.   ./(#(,.., .      ..    ,    .,,.,,..  .     *.    #@@
+,                                   ,*,#**(#  @* /                                                             #&, .@@*                      ..*#/@&&**.     .,*,*,,/,,.     ....                ,,  /@@
+,                                     */. #%./@ ,#                                                              #@,  .@#                            .*/(#&%(/,. .     .,*.,..   ....                 .@@
+,                                          ,&#&,%#   (*                                                          ,@*  /&#                                    .,,((&%#*,.     ......           .*      %@
+,..                            .              /%(( #@.                                            ./@/*,/(%#/,.*(%#@@,.&(&/((#&&@@@&@%#%#/(,                          ...%@&//,        .,       #     .@
+,..       .                     ,#,  (/      #%&(/.&                                    .((&@@&&%(****,.   .  #&@@&   &&  /,  ,     ,,//*/*/&#%*(((((#/(#*(/*.                .,*/%&#,         (*      &
+,                           ,(&,. *.(..&&%((.      //,                         .*(&&%%(/*   ..            #%, *&.(*. #**#.....,.                .,.*,,*/*/#/*,,,,*.(%(,,*..           .*/%/***,(.      (
+,                              /&*&*,/ #*    *@%*   .. /                .*#@&#,,                ...,/#@@@@&@@&&%(/.*#(*#(#(/(/*#/(/*(%&@%@@@@&/(,/**./((*.  ,,.   ,**,,**.**/#&#,.                     ,
+,       ..                   #@@(.%#@.  .*@(  ./#(.##.   .,(&%%#&@@@@(,*(*.           ..*#%@@@@@@&,                                              ,*/@%#&%,..   ,(/(***.   ..       ./((*.              .
+*                                *&.   ,&@@@@/*#%(.      ./(%&(*.//*       .    */&&@@@@&(,.                                                                 .,/#&&#*      .,,.            ,(((*       .
+*               . ...                               .(@@&/**/*.  .,.. . .,/#@@@&%#..                                                                              ..*/##@%##,                    ,/**. .
+,                      .                        .%@&#,/.  ... **,../@@@%/*..                                                                                                .../#*,*.                 .*
+,                            .              /@@@@/,  * **. ,&@@@@(..                                                                                                                 .*///,            .
+,                                  .   .,(@@&#*    ,#@&@@%#,                    .                                                                                                          ..,(/*,.    .
+,   .                                         **&@@@&(.                           ..                                                                                                               .,(#(
+,                                            .@(.                                                                                                                                                      .")
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SayMyNameAgain")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SayMyNameAgain")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d765aa44-7a5c-427b-a3a6-984fd8c7cde2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
